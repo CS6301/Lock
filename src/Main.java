2a743002d9a15d0f9797a604272bf04ad61f138f
Hanlin He <hanlhe@tetrationanalytics.com>
2018-09-18 17:05:46 -0500

    Add timer output.

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,42 @@
 import cs6301.github.io.lock.Lock;
 import cs6301.github.io.lock.TournamentPetersonLock;
 import cs6301.github.io.test.TestLock;
+import cs6301.github.io.test.Timer;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        int threadNum = Integer.parseInt(args[0]);
+        int threadNum = 2;
+        int iteration = 100;
+        boolean verbose = false;
+        if (args.length >= 1)
+            threadNum = Integer.parseInt(args[0]);
+        if (args.length >= 2)
+            iteration = Integer.parseInt(args[1]);
+
+        if (args[args.length - 1].endsWith("verbose"))
+            verbose = true;
 
         Lock lock = new TournamentPetersonLock(threadNum);
 
         Thread[] threads = new Thread[threadNum];
         for (int i = 0; i < threadNum; i++) {
-            threads[i] = new Thread(new TestLock(lock, i, 500 * i));
+            threads[i] = new Thread(new TestLock(lock, i, iteration));
         }
+        if (verbose) {
+            System.out.printf("Running %d thread with %d iteration.\n", threadNum, iteration);
+        }
+        TestLock.reset();
+        Timer timer = new Timer();
         for (Thread t : threads)
             t.start();
+
+        for (Thread t : threads)
+            t.join();
+        System.out.println(timer.end());
+        if (verbose) {
+            System.out.printf("Shared value after execution: %d", TestLock.shared);
+        }
     }
 }
--- src/cs6301/github/io/test/TestLock.java
+++ src/cs6301/github/io/test/TestLock.java
@@ -4,16 +4,20 @@ import cs6301.github.io.lock.Lock;
 
 public class TestLock implements Runnable {
 
-    static int shared = 0;
+    public static int shared = 0;
+
+    public static void reset() {
+        shared = 0;
+    }
 
     private Lock lock;
     private int id;
     private int iteration;
 
-    public TestLock(Lock lock, int id, int iterations) {
+    public TestLock(Lock lock, int id, int iteration) {
         this.lock = lock;
         this.id = id;
-        this.iteration = iterations;
+        this.iteration = iteration;
     }
 
     @Override
@@ -21,7 +25,6 @@ public class TestLock implements Runnable {
         for (int i = 0; i < this.iteration; i++) {
             this.lock.lock();
             shared++;
-            System.out.println(this.id + " increment shared value to " + shared);
             this.lock.unlock();
         }
     }
